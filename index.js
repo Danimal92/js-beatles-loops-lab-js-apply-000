65f04789aab5812337b55f5391675cbd2588973f
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-07-22 19:27:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,22 @@ function theBeatlesPlay(musicians, instruments){
   }
   
   
+function johnLennonFacts(){
   
+  
+}
+    it("returns an array of strings with exclamation points", function() {
+      expect(johnLennonFacts([
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
+
+      expect(johnLennonFacts([
+        "foo",
+        "bar",
+      ])).toEqual(["foo!!!", "bar!!!"])
+    });
+  });
   
